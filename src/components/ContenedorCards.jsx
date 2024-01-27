7203c92fb002bf16ca68f51536924142a4064ef2
emir paradi <emir.alejandro.paradi@gmail.com>
2024-01-27 19:33:53 -0300

    Logica terminada, el contenido del array se guarda correctamente en localStorage

--- src/components/ContenedorCards.jsx
+++ src/components/ContenedorCards.jsx
@@ -1,12 +1,22 @@
+//2 -Desde Contenedor Cards.jsx
+
 import CardsPaciente from "./CardPaciente";
 
-const ContenedorCards = () => {
+const ContenedorCards = ({
+  arrayPacientes,
+  borrarPaciente,
+  nombrePaciente,
+}) => {
   return (
     <section className="mb-5 d-flex justify-content-around row mt-5">
-      <CardsPaciente></CardsPaciente>
-      <CardsPaciente></CardsPaciente>
-      <CardsPaciente></CardsPaciente>
-      <CardsPaciente></CardsPaciente>
+      {arrayPacientes.map((paciente, indicePaciente) => (
+        <CardsPaciente
+          key={indicePaciente}
+          paciente={paciente}
+          borrarPaciente={borrarPaciente}
+          nombrePaciente={paciente.nombre}
+        ></CardsPaciente>
+      ))}
     </section>
   );
 };
--- src/components/FormularioPacientes.jsx
+++ src/components/FormularioPacientes.jsx
@@ -1,10 +1,56 @@
+//1 - Desde FormularioPacientes.jsx
 import { Button, Form } from "react-bootstrap";
 import ContenedorCards from "./ContenedorCards";
+import { useState, useEffect } from "react";
 
 const FormularioPacientes = () => {
+  /* Variables globales -------------------- */
+  const [paciente, setPaciente] = useState({
+    nombre: "",
+    nombreDuenio: "",
+    fecha: "",
+    hora: "",
+    sintomas: "",
+  });
+  const arrayPacientesLocalStorage =
+    JSON.parse(localStorage.getItem("pacientesKey")) || [];
+  const [arrayPacientes, setArrayPacientes] = useState(
+    arrayPacientesLocalStorage
+  );
+
+  /* Funciones ------------------------------ */
+
+  const manejadorSubmit = (e) => {
+    e.preventDefault();
+
+    setArrayPacientes([...arrayPacientes, paciente]);
+    setPaciente({
+      nombre: "",
+      nombreDuenio: "",
+      fecha: "",
+      hora: "",
+      sintomas: "",
+    });
+  };
+
+  const borrarPaciente = (nombrePaciente) => {
+    const copiaArrayPaciente = arrayPacientes.filter(
+      (paciente) => paciente.nombre !== nombrePaciente
+    );
+    setArrayPacientes(copiaArrayPaciente);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("pacientesKey", JSON.stringify(arrayPacientes));
+  }, [arrayPacientes]);
+
+  /* Maquetado Lógica extra ---------------------------- */
   return (
     <>
-      <Form className="border px-5 py-5 rounded-1 bg-body-tertiary border-danger">
+      <Form
+        className="border px-5 py-5 rounded-1 bg-body-tertiary border-danger"
+        onSubmit={manejadorSubmit}
+      >
         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
           <div className="d-flex mb-3 align-items-center justify-content-between">
             <Form.Label>Nombre de la mascota:</Form.Label>
@@ -12,6 +58,13 @@ const FormularioPacientes = () => {
               type="text"
               placeholder="Ej. Roco"
               className="color-inputs anchoControls"
+              required
+              maxLength={25}
+              minLength={2}
+              onChange={(e) =>
+                setPaciente({ ...paciente, nombre: e.target.value })
+              }
+              value={paciente.nombre}
             />
           </div>
           <div className="d-flex mb-3 align-items-center justify-content-between">
@@ -20,6 +73,13 @@ const FormularioPacientes = () => {
               type="text"
               placeholder="Ej. Juan"
               className="color-inputs anchoControls"
+              minLength={3}
+              maxLength={25}
+              required
+              onChange={(e) =>
+                setPaciente({ ...paciente, nombreDuenio: e.target.value })
+              }
+              value={paciente.nombreDuenio}
             />
           </div>
           <div className="d-flex align-items-center mb-3 justify-content-between">
@@ -28,12 +88,22 @@ const FormularioPacientes = () => {
               type="date"
               placeholder="dd/mm/yyyy"
               className="color-inputs anchoInputsTime"
+              required
+              onChange={(e) =>
+                setPaciente({ ...paciente, fecha: e.target.value })
+              }
+              value={paciente.fecha}
             />
             <Form.Label>Hora:</Form.Label>
             <Form.Control
               type="time"
               placeholder="name@example.com"
               className="color-inputs w-auto"
+              required
+              onChange={(e) =>
+                setPaciente({ ...paciente, hora: e.target.value })
+              }
+              value={paciente.hora}
             />
           </div>
           <div className="d-flex mb-3 align-items-center justify-content-between">
@@ -42,14 +112,26 @@ const FormularioPacientes = () => {
               type="text"
               placeholder="describir sintomas"
               className="color-inputs anchoControls "
+              minLength={4}
+              maxLength={50}
+              required
+              onChange={(e) =>
+                setPaciente({ ...paciente, sintomas: e.target.value })
+              }
+              value={paciente.sintomas}
             />
           </div>
         </Form.Group>
         <div className="text-end mt-5">
-          <Button variant="outline-secondary">Enviar</Button>
+          <Button variant="outline-secondary" type="submit">
+            Enviar
+          </Button>
         </div>
       </Form>
-      <ContenedorCards></ContenedorCards>
+      <ContenedorCards
+        arrayPacientes={arrayPacientes}
+        borrarPaciente={borrarPaciente}
+      ></ContenedorCards>
     </>
   );
 };
